62af3332215a79065963904ab4e23c01b03f5202
Edson13092000 <edsonchoque65@gmail.com>
2025-12-10 16:39:53 -0500

    Update Delivery.java

--- SistemaEcommerce/src/ecommerce/Delivery.java
+++ SistemaEcommerce/src/ecommerce/Delivery.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ecommerce;
 
-/**
- *
- * @author Jenner Jordy
- */
-    
 
 public class Delivery extends Usuario {
     private String direccion;
@@ -23,4 +15,5 @@ public class Delivery extends Usuario {
     public String toString() {
         return "ID "+getIdUsuario()+" Delivery: " + getNombre() + " (" + getEmail() + ") Telefono: "+getTelefono();
     }
+
 }
