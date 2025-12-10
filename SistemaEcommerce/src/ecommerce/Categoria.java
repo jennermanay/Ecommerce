e03756e8bad3e2da2569875c238e6fb6566eb22f
Edson13092000 <edsonchoque65@gmail.com>
2025-12-10 16:31:33 -0500

    clase categoria

---
+++ SistemaEcommerce/src/ecommerce/Categoria.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ecommerce;
+
+/**
+ *
+ * @author Jenner Jordy
+ */
+public class Categoria {
+ private int idCategoria;
+    private String nombreCategoria;
+
+    public Categoria(int idCategoria, String nombreCategoria) {
+        this.idCategoria = idCategoria;
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    public int getIdCategoria() { return idCategoria; }
+    public String getNombreCategoria() { return nombreCategoria; }
+    
+    @Override
+    public String toString() {
+        return idCategoria + ": " + nombreCategoria;
+    }
+}
