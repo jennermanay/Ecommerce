256ef17cae9ff07880b19c3d6bd1d09ecc6911ac
jennermanay <jennerjordy@gmail.com>
2025-12-10 16:27:28 -0500

    Update Producto.java

--- Ecommerce/src/ecommerce/Producto.java
+++ Ecommerce/src/ecommerce/Producto.java
@@ -9,28 +9,32 @@ package ecommerce;
  * @author Jenner Jordy
  */
 public class Producto {
-    private int idProducto;
+ private int idProducto;
     private String nombreProducto;
     private String descripcion;
     private double precio;
     private int stock;
+    private int idCategoria; 
 
-    public Producto(int idProducto, String nombreProducto, String descripcion, double precio, int stock) {
+    public Producto(int idProducto, String nombreProducto, String descripcion, double precio, int stock, int idCategoria) {
         this.idProducto = idProducto;
         this.nombreProducto = nombreProducto;
         this.descripcion = descripcion;
         this.precio = precio;
         this.stock = stock;
+        this.idCategoria = idCategoria;
     }
 
     public int getIdProducto() { return idProducto; }
     public String getNombreProducto() { return nombreProducto; }
     public double getPrecio() { return precio; }
     public int getStock() { return stock; }
+    public String getDescripcion() { return descripcion; }
+    public int getIdCategoria() { return idCategoria; }
     public void setStock(int stock) { this.stock = stock; }
-    
+
     @Override
     public String toString() {
-        return idProducto + ". " + nombreProducto + " - $" + precio + " (Stock: " + stock + ")";
+        return "ID: " + idProducto + " | " + nombreProducto + " | $" + precio + " | Stock: " + stock + " | Cat ID: " + idCategoria;
     }
 }
