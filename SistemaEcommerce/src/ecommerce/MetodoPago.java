738b5d3ecea581bb1e6b317e859fa26fe26d4573
Edson13092000 <edsonchoque65@gmail.com>
2025-12-10 16:41:45 -0500

    Update MetodoPago.java

--- SistemaEcommerce/src/ecommerce/MetodoPago.java
+++ SistemaEcommerce/src/ecommerce/MetodoPago.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ecommerce;
 
-/**
- *
- * @author Jenner Jordy
- */
 public class MetodoPago {
   private int idMetodoPago;
     private String nombreMetodo;
@@ -29,4 +22,5 @@ public class MetodoPago {
     public String toString() {
         return idMetodoPago + " - " + nombreMetodo + " (" + numeroCuenta + ")";
     }
+
 }
